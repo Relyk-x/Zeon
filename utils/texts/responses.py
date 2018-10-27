dba1059260628b825165cc5613ddef4b6261597a
Relyk-x <43947707+Relyk-x@users.noreply.github.com>
2018-10-27 09:51:09 +0800

    Update responses.py

--- utils/texts/responses.py
+++ utils/texts/responses.py
@@ -1,1 +1,22 @@
+chooseresponse = ["I choose **{c}**", "I pick **{c}**", "**{c}** is the best choice", "**{c}** is my choice", "**{c}** of course!"]
 
+ballresponse = [
+  'Yes', 'No', 'Take a wild guess...', 'Very doubtful',
+  'Sure', 'Without a doubt', 'Most likely', 'Might be possible',
+  "You'll be the judge", 'no... (╯°□°）╯︵ ┻━┻', 'no... baka',
+  'senpai, pls no ;-;'
+]
+
+cloneresponse = [
+  'Yes', 'No', 'Take a wild guess...', 'How did you know?',
+  'Heck off', 'pffft... no?', 'Do you really think that?', 'no :(',
+  'shush, don\'t say it so loud!'
+]
+
+drunkresponse = [
+    "Life is not a fairy tale. If you lose your shoe at midnight, you're drunk",
+    "Me? Drunk? Pffft.. no?", "No your drunk \*hick\*", "what is life?",
+    "I wanna get so drunk..", "Party is life!!1", "I drank so much vodka last night, I woke up with a russian accent",
+    "Drunk ~~is love~~\nDrunk. Just drunk.", "A man's true character comes out when he's drunk.", "https://i.alexflipnote.xyz/5dcc3a.png",
+    "Get drunk they said.. it will be fun they said..", "🍻"
+]
